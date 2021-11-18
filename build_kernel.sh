ce60ea4beaa3194ae6d62571c868e8b55f5d20e8
Adil Hanney <adilh+github@orchid.anonaddy.com>
2021-11-18 23:42:40 +0000

    build_kernel.sh: Set localversion to -adil

--- build_kernel.sh
+++ build_kernel.sh
@@ -6,7 +6,7 @@ mkdir out
 BUILD_CROSS_COMPILE=$(pwd)/toolchain/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 KERNEL_LLVM_BIN=$(pwd)/toolchain/llvm-arm-toolchain-ship/10.0/bin/clang
 CLANG_TRIPLE=aarch64-linux-gnu-
-KERNEL_MAKE_ENV=""
+KERNEL_MAKE_ENV="LOCALVERSION=-adil"
 
 make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE vendor/gts7lwifi_eur_open_defconfig
 make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE
